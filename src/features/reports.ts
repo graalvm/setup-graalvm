2ab0f262f84820e436e1ee270e6e6c008a5bd89d
jessica <pauljessica2001@gmail.com>
2023-12-09 18:50:44 +0100

    add createPrReport for comparison with base branch

--- src/features/reports.ts
+++ src/features/reports.ts
@@ -145,7 +145,9 @@ export async function generateReports(): Promise<void> {
       await core.summary.write()
     }
     if (arePRReportsEnabled()) {
-      await createPRComment(report)
+      const baseBranchBuildOutput: BuildOutput = JSON.parse(await getPrBaseBranchMetrics())
+      const prReport = creatPRReport(buildOutput, baseBranchBuildOutput)
+      await createPRComment(prReport)
     }
 
     const treeSha = await createTree(JSON.stringify(buildOutput))
@@ -481,6 +483,7 @@ function createReport(data: BuildOutput): string {
       <th align="right">Size</th>
       <th align="right">in %</th>
       <th align="left">Details</th>
+      <th 
     </tr>
   </thead>
   <tbody>
@@ -492,6 +495,7 @@ function createReport(data: BuildOutput): string {
       details.total_bytes
   )}</td>
       <td align="left">${details.code_area.compilation_units.toLocaleString()} compilation units</td>
+      <td></td>
     </tr>
     <tr>
       <td align="left"><a href="${DOCS_BASE}#glossary-image-heap" target="_blank">Image heap</a></td>
@@ -576,4 +580,278 @@ function secondsToHuman(seconds: number): string {
   } else {
     return `${Math.trunc(seconds / 60)}m ${Math.trunc(seconds % 60)}s`
   }
+}
+
+function creatPRReport(recentData: BuildOutput, baseBranchData: BuildOutput): string {
+  const context = github.context
+  const baseBranch = process.env.GITHUB_BASE_REF
+  const recentBranch = process.env.GITHUB_HEAD_REF
+  const info = recentData.general_info
+  const analysis = recentData.analysis_results
+  const analysisTypes = analysis.types ? analysis.types : analysis.classes
+  const recentDetails = recentData.image_details
+  const baseBranchDetails = recentData.image_details
+  let objectCount = ''
+  if (recentDetails.image_heap.objects) {
+    objectCount = `${recentDetails.image_heap.objects.count.toLocaleString()} objects, `
+  }
+  const debugInfoBytes = recentDetails.debug_info ? recentDetails.debug_info.bytes : 0
+  const recentOtherBytes =
+      recentDetails.total_bytes -
+      recentDetails.code_area.bytes -
+      recentDetails.image_heap.bytes -
+      debugInfoBytes
+  const baseBranchOtherBytes =
+      baseBranchDetails.total_bytes -
+      baseBranchDetails.code_area.bytes -
+      baseBranchDetails.image_heap.bytes -
+      debugInfoBytes
+  let debugInfoLine = ''
+  if (recentDetails.debug_info) {
+    debugInfoLine = `
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-debug-info" target="_blank">Debug info</a></td>
+      <td align="right">${bytesToHuman(debugInfoBytes)}</td>
+      <td align="right">${toPercent(debugInfoBytes, recentDetails.total_bytes)}</td>
+      <td align="left"></td>
+    </tr>`
+  }
+  let versionLine
+  if (info.vendor_version) {
+    versionLine = `
+    <tr>
+      <td><a href="${DOCS_BASE}#glossary-java-info" target="_blank">Java version</a></td>
+      <td>${info.java_version}</td>
+      <td><a href="${DOCS_BASE}#glossary-java-info" target="_blank">Vendor version</a></td>
+      <td>${info.vendor_version}</td>
+    </tr>`
+  } else {
+    versionLine = `
+    <tr>
+      <td><a href="${DOCS_BASE}#glossary-version-info" target="_blank">GraalVM version</a></td>
+      <td>${info.graalvm_version}</td>
+      <td><a href="${DOCS_BASE}#glossary-java-version-info" target="_blank">Java version</a></td>
+      <td>${info.java_version}</td>
+    </tr>`
+  }
+  let graalLine
+  if (info.graal_compiler) {
+    let pgoSuffix = ''
+    const isOracleGraalVM =
+        info.vendor_version && info.vendor_version.includes('Oracle GraalVM')
+    if (isOracleGraalVM) {
+      const pgo = info.graal_compiler.pgo
+      const pgoText = pgo ? pgo.join('+') : 'off'
+      pgoSuffix = `, <a href="${DOCS_BASE}#recommendation-pgo" target="_blank">PGO</a>: ${pgoText}`
+    }
+    graalLine = `
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-graal-compiler" target="_blank">Graal compiler</a></td>
+      <td colspan="3">
+        optimization level: ${info.graal_compiler.optimization_level},
+        target machine: ${info.graal_compiler.march}${pgoSuffix}
+      </td>
+    </tr>`
+  }
+
+  const recentResources = recentData.resource_usage
+  const baseBranchResources = baseBranchData.resource_usage
+
+  let totalTime = ''
+  let gcTotalTimeRatio = ''
+  if (recentResources.total_secs) {
+    totalTime = ` in ${secondsToHuman(recentResources.total_secs)}`
+    gcTotalTimeRatio = ` (${toPercent(
+        recentResources.garbage_collection.total_secs,
+        recentResources.total_secs
+    )} of total time)`
+  }
+
+  return `## GraalVM Native Image Build Report
+
+\`${info.name}\` generated${totalTime} as part of the '${
+      context.job
+  }' job in run <a href="${context.serverUrl}/${context.repo.owner}/${
+      context.repo.repo
+  }/actions/runs/${context.runId}" target="_blank">#${context.runNumber}</a>.
+
+#### Environment
+
+<table>${versionLine}${graalLine}
+  <tr>
+    <td><a href="${DOCS_BASE}#glossary-ccompiler" target="_blank">C compiler</a></td>
+    <td colspan="3">${info.c_compiler}</td>
+  </tr>
+  <tr>
+    <td><a href="${DOCS_BASE}#glossary-gc" target="_blank">Garbage collector</a></td>
+    <td colspan="3">${info.garbage_collector}</td>
+  </tr>
+</table>
+
+#### Analysis Results
+
+<table>
+  <thead>
+    <tr>
+      <th align="left">Category</th>
+      <th align="right">Types</th>
+      <th align="right">in %</th>
+      <th align="right">Fields</th>
+      <th align="right">in %</th>
+      <th align="right">Methods</th>
+      <th align="right">in %</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-reachability" target="_blank">Reachable</a></td>
+      <td align="right">${analysisTypes.reachable.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysisTypes.reachable,
+      analysisTypes.total
+  )}</td>
+      <td align="right">${analysis.fields.reachable.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.fields.reachable,
+      analysis.fields.total
+  )}</td>
+      <td align="right">${analysis.methods.reachable.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.methods.reachable,
+      analysis.methods.total
+  )}</td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-reflection-registrations" target="_blank">Reflection</a></td>
+      <td align="right">${analysisTypes.reflection.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysisTypes.reflection,
+      analysisTypes.total
+  )}</td>
+      <td align="right">${analysis.fields.reflection.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.fields.reflection,
+      analysis.fields.total
+  )}</td>
+      <td align="right">${analysis.methods.reflection.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.methods.reflection,
+      analysis.methods.total
+  )}</td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-jni-access-registrations" target="_blank">JNI</a></td>
+      <td align="right">${analysisTypes.jni.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysisTypes.jni,
+      analysisTypes.total
+  )}</td>
+      <td align="right">${analysis.fields.jni.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.fields.jni,
+      analysis.fields.total
+  )}</td>
+      <td align="right">${analysis.methods.jni.toLocaleString()}</td>
+      <td align="right">${toPercent(
+      analysis.methods.jni,
+      analysis.methods.total
+  )}</td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-reachability" target="_blank">Loaded</a></td>
+      <td align="right">${analysisTypes.total.toLocaleString()}</td>
+      <td align="right">100.000%</td>
+      <td align="right">${analysis.fields.total.toLocaleString()}</td>
+      <td align="right">100.000%</td>
+      <td align="right">${analysis.methods.total.toLocaleString()}</td>
+      <td align="right">100.000%</td>
+    </tr>
+  </tbody>
+</table>
+
+#### Image Details
+
+<table>
+  <thead>
+    <tr>
+      <th align="left">Category</th>
+      <th align="right">Size</th>
+      <th align="right">in %</th>
+      <th align="left">Details</th>
+      <th align="left">Compared to <i>${baseBranch}</i> (+/- x Bytes)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-code-area" target="_blank">Code area</a></td>
+      <td align="right">${bytesToHuman(recentDetails.code_area.bytes)}</td>
+      <td align="right">${toPercent(
+      recentDetails.code_area.bytes,
+      recentDetails.total_bytes
+  )}</td>
+      <td align="left" ${(recentDetails.code_area.bytes - baseBranchDetails.code_area.bytes) < 0 ? 'style="background-color: green;"': ''}${(recentDetails.code_area.bytes - baseBranchDetails.code_area.bytes) > 0 ? 'style="background-color: red;"': ''}>${bytesToHuman(recentDetails.code_area.bytes - baseBranchDetails.code_area.bytes)} (${toPercent(baseBranchDetails.code_area.bytes, recentDetails.code_area.bytes)})</td>
+      <td align="left">${recentDetails.code_area.compilation_units.toLocaleString()} compilation units</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-image-heap" target="_blank">Image heap</a></td>
+      <td align="right">${bytesToHuman(recentDetails.image_heap.bytes)}</td>
+      <td align="right">${toPercent(
+      recentDetails.image_heap.bytes,
+      recentDetails.total_bytes
+  )}</td>
+      <td align="left" ${(recentDetails.image_heap.bytes - baseBranchDetails.image_heap.bytes) < 0 ? 'style="background-color: green;"': ''}${(recentDetails.image_heap.bytes - baseBranchDetails.image_heap.bytes) > 0 ? 'style="background-color: red;"': ''}>${bytesToHuman(recentDetails.image_heap.bytes - baseBranchDetails.image_heap.bytes)} (${toPercent(baseBranchDetails.image_heap.bytes, recentDetails.image_heap.bytes)})</td>
+      <td align="left">${objectCount}${bytesToHuman(
+      recentDetails.image_heap.resources.bytes
+  )} for ${recentDetails.image_heap.resources.count.toLocaleString()} resources</td>
+    </tr>${debugInfoLine}
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-other-data" target="_blank">Other data</a></td>
+      <td align="right">${bytesToHuman(recentOtherBytes)}</td>
+      <td align="right">${toPercent(recentOtherBytes, recentDetails.total_bytes)}</td>
+      <td align="left" ${(recentOtherBytes - baseBranchOtherBytes) < 0 ? 'style="background-color: green;"': ''}${(recentOtherBytes - baseBranchOtherBytes) > 0 ? 'style="background-color: red;"': ''}>${bytesToHuman(recentOtherBytes - baseBranchOtherBytes)} (${toPercent(baseBranchOtherBytes, recentOtherBytes)})</td>
+      <td align="left"></td>
+    </tr>
+    <tr>
+      <td align="left">Total</td>
+      <td align="right"><strong>${bytesToHuman(
+      recentDetails.total_bytes
+  )}</strong></td>
+      <td align="right">100.000%</td>
+      <td align="left" ${(recentDetails.total_bytes - baseBranchDetails.total_bytes) < 0 ? 'style="background-color: green;"': ''}${(recentDetails.total_bytes - baseBranchDetails.total_bytes) > 0 ? 'style="background-color: red;"': ''}>${bytesToHuman(recentDetails.total_bytes - baseBranchDetails.total_bytes)} (${toPercent(baseBranchDetails.total_bytes, recentDetails.total_bytes)})</td>
+      <td align="left"></td>
+    </tr>
+  </tbody>
+</table>
+
+#### Resource Usage
+
+<table>
+  <tbody>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-garbage-collections" target="_blank">Garbage collection</a></td>
+      <td align="left">${recentResources.garbage_collection.total_secs.toFixed(
+      2
+  )}s${gcTotalTimeRatio} in ${recentResources.garbage_collection.count} GCs</td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-peak-rss" target="_blank">Peak RSS</a></td>
+      <td align="left">${bytesToHuman(
+      recentResources.memory.peak_rss_bytes
+  )} (${toPercent(
+      recentResources.memory.peak_rss_bytes,
+      recentResources.memory.system_total
+  )} of ${bytesToHuman(recentResources.memory.system_total)} system memory)</td>
+    </tr>
+    <tr>
+      <td align="left"><a href="${DOCS_BASE}#glossary-cpu-load" target="_blank">CPU load</a></td>
+      <td align="left">${recentResources.cpu.load.toFixed(3)} (${toPercent(
+      recentResources.cpu.load,
+      recentResources.cpu.total_cores
+  )} of ${recentResources.cpu.total_cores} CPU cores)</td>
+    </tr>
+  </tbody>
+</table>
+
+<em>Report generated by <a href="https://github.com/marketplace/actions/github-action-for-graalvm" target="_blank">setup-graalvm</a>.</em>`
 }
