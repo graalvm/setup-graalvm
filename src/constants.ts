15aa83eb0c6078f195b4361efc59f1d0e3af157f
Fabio Niephaus <fabio.niephaus@oracle.com>
2024-10-28 15:50:28 +0100

    Bump version to `1.2.5`.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import * as otypes from '@octokit/types'
 
-export const ACTION_VERSION = '1.2.4'
+export const ACTION_VERSION = '1.2.5'
 
 export const INPUT_VERSION = 'version'
 export const INPUT_GDS_TOKEN = 'gds-token'
