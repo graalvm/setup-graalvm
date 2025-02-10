120f15af1d1aac136f9f00614a63249e201a5d42
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-02-10 09:04:34 +0100

    Update imports.

--- src/features/sbom.ts
+++ src/features/sbom.ts
@@ -1,11 +1,11 @@
-import * as c from '../constants'
+import * as c from '../constants.js'
 import * as core from '@actions/core'
 import * as fs from 'fs'
 import * as github from '@actions/github'
 import * as glob from '@actions/glob'
 import {basename} from 'path'
 import * as semver from 'semver'
-import {setNativeImageOption} from '../utils'
+import {setNativeImageOption} from '../utils.js'
 
 const INPUT_NI_SBOM = 'native-image-enable-sbom'
 const SBOM_FILE_SUFFIX = '.sbom.json'
