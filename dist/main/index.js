288f9fa79d47b5b8e2ff73227077919d1538a96c
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-02-17 08:39:56 +0100

    Update dist files.

--- dist/main/index.js
+++ dist/main/index.js
@@ -42776,7 +42776,7 @@ var RequestError = class extends Error {
     if (options.request.headers.authorization) {
       requestCopy.headers = Object.assign({}, options.request.headers, {
         authorization: options.request.headers.authorization.replace(
-          / .*$/,
+          /(?<! ) .*$/,
           " [REDACTED]"
         )
       });
