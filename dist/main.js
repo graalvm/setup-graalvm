2a2412009026a83f51d179f92dc2b3fd4c8142df
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-10-02 10:21:23 +0200

    Bump version to `1.4.1`.

--- dist/main.js
+++ dist/main.js
@@ -39,7 +39,7 @@ import * as https from 'node:https';
 import * as zlib from 'node:zlib';
 import require$$1$5 from 'tty';
 
-const ACTION_VERSION = '1.4.0';
+const ACTION_VERSION = '1.4.1';
 const INPUT_VERSION = 'version';
 const INPUT_GDS_TOKEN = 'gds-token';
 const INPUT_JAVA_VERSION = 'java-version';
