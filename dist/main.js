fd827845863c95a93cb0a6837fcf0df6528782de
Fabio Niephaus <fabio.niephaus@oracle.com>
2026-06-15 15:45:51 +0200

    Bump version to `1.5.5`.

--- dist/main.js
+++ dist/main.js
@@ -51,7 +51,7 @@ import require$$1$5 from 'tty';
 import { createHmac } from 'node:crypto';
 import fs$1 from 'node:fs';
 
-const ACTION_VERSION = '1.5.4';
+const ACTION_VERSION = '1.5.5';
 const INPUT_VERSION = 'version';
 const INPUT_GDS_TOKEN = 'gds-token';
 const INPUT_JAVA_VERSION = 'java-version';
