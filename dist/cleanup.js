86845609d7e7e52ba393b41639ec926d628dc35f
Fabio Niephaus <fabio.niephaus@oracle.com>
2026-07-03 15:42:00 +0200

    Bump version to `1.6.0`.

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -30134,7 +30134,7 @@ function getState(name) {
     return process.env[`STATE_${name}`] || '';
 }
 
-const ACTION_VERSION = '1.5.6';
+const ACTION_VERSION = '1.6.0';
 const INPUT_GITHUB_TOKEN = 'github-token';
 const INPUT_CACHE = 'cache';
 process.platform === 'linux';
