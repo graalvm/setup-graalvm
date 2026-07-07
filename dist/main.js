02ee31b01fd0710d7745ad79c13eb18c99e020e5
Fabio Niephaus <fabio.niephaus@oracle.com>
2026-07-07 18:28:49 +0200

    Set `responseFields=metadata` explicitly.

--- dist/main.js
+++ dist/main.js
@@ -38681,7 +38681,7 @@ async function fetchArtifact(userAgent, graalVMVersion, jdkVersion) {
         majorJavaVersion = jdkVersion;
     }
     const catalogOS = IS_MACOS ? 'macos' : GRAALVM_PLATFORM;
-    const requestUrl = `${GDS_BASE}/artifacts?productId=${GDS_GRAALVM_PRODUCT_ID}&displayName=Oracle%20GraalVM&metadata=java:jdk${majorJavaVersion}&metadata=os:${catalogOS}&metadata=arch:${GRAALVM_ARCH}&metadata=isBase:True&status=PUBLISHED&responseFields=id&responseFields=checksum&sortBy=m:version&sortOrder=DESC`;
+    const requestUrl = `${GDS_BASE}/artifacts?productId=${GDS_GRAALVM_PRODUCT_ID}&displayName=Oracle%20GraalVM&metadata=java:jdk${majorJavaVersion}&metadata=os:${catalogOS}&metadata=arch:${GRAALVM_ARCH}&metadata=isBase:True&status=PUBLISHED&responseFields=id&responseFields=checksum&responseFields=metadata&sortBy=m:version&sortOrder=DESC`;
     debug(`Requesting ${requestUrl}`);
     const response = await http.get(requestUrl, { accept: 'application/json' });
     if (response.message.statusCode !== 200) {
